2a98147d0505062d365025aa970fe26b7e58e3f5
Jorge Fajardo <jorgefavi96@gmail.com>
2022-02-11 02:33:10 +0100

    docs agregado

---
+++ docs/precache-manifest.e688d927433d5c187d306f2e9f9f03bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86501c65855ac18caba9c183bb7e4b90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9d664f8573f87469889",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d664f8573f87469889",
+    "url": "/static/js/main.59c1a82d.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
